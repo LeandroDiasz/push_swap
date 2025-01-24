0e083d4177e58907468deb866586f5b721582ae7
ledias-d <leocraftjg@gmai.com>
2025-01-24 20:07:30 -0300

    feat: add error handling and stack freeing functions in erro.c

---
+++ src/erro.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   erro.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ledias-d <ledias-d@student.42.rio>         #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-01-24 18:31:40 by ledias-d          #+#    #+#             */
+/*   Updated: 2025-01-24 18:31:40 by ledias-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	error(t_stack **stack)
+{
+	if (stack)
+		free_stack(stack);
+	ft_putstr_fd("Error\n", 2);
+	exit(EXIT_FAILURE);
+}
+void	free_stack(t_stack **stack)
+{
+	t_stack *tmp;
+
+	tmp = NULL;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+}
