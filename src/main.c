51fc84931bff65454acbc22a8cda8797c7ec8a5d
ledias-d <leocraftjg@gmai.com>
2025-01-24 20:08:39 -0300

    feat: add main function with stack_a initialization and input validations

---
+++ src/main.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ledias-d <ledias-d@student.42.rio>         #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-01-16 19:36:25 by ledias-d          #+#    #+#             */
+/*   Updated: 2025-01-16 19:36:25 by ledias-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+#include <stdio.h>
+
+void	init_stack(t_stack **a, char **av)
+{
+	int		i;
+	long	nbr;
+
+	i = 0;
+	while (av[i])
+	{
+		if (!verify_sintax(av[i]))
+			error(a);
+		nbr = ft_atol(av[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			error(a);
+		if (!verify_duplicate(*a, (int)nbr))
+			error(a);
+		lstadd_back(a, lstnew(nbr));
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	t_stack *stack_a;
+	t_stack *stack_b;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	if (ac == 1 || (ac == 2 && !av[1][0]))
+		return (1);
+	else if (ac == 2)
+		av = ft_split(av[1], ' ');
+	else
+		av++;
+	init_stack(&stack_a, av);
+	/*if (!is_sorted(stack_a))
+	{
+		
+	}*/
+	return (0);
+}
