ee0221c476e98107d815dce9b3d7c6904193e54b
ledias-d <leocraftjg@gmai.com>
2025-01-30 19:18:43 -0300

    feat: add function to free a matrix in erro.c

--- src/erro.c
+++ src/erro.c
@@ -33,3 +33,13 @@ void	free_stack(t_stack **stack)
 		*stack = tmp;
 	}
 }
+
+void    free_matrix(char **str)
+{
+    int	i;
+	
+	i = 0;
+    while (str[i])
+        free(str[i++]);
+    free(str);
+}
