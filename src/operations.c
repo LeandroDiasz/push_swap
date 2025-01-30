70924f38e8c59636bac5c1c7f3fa8e24e20ad0b1
ledias-d <leocraftjg@gmai.com>
2025-01-30 19:21:20 -0300

    fix: update function prototypes in operations.c to receive t_stack **stack instead of t_stack *stack

--- src/operations.c
+++ src/operations.c
@@ -12,16 +12,7 @@
 
 #include "../push_swap.h"
 
-void	node_swap(t_stack *stack)
-{
-	t_stack *tmp;
-
-	tmp = stack;
-	stack = stack->next;
-	stack->next = tmp;
-}
-
-void	swap(t_stack *stack_a, t_stack *stack_b, char set)
+void	swap(t_stack **stack_a, t_stack **stack_b, char set)
 {
 	if (set == DO_A)
 	{
@@ -40,56 +31,58 @@ void	swap(t_stack *stack_a, t_stack *stack_b, char set)
 		ft_putstr("ss\n");
 	}
 }
-void	push(t_stack *stack_a, t_stack *stack_b, char set)
+void	push(t_stack **stack_a, t_stack **stack_b, char set)
 {
 	if (set == DO_A)
 	{
-		lstadd_front(&stack_a, stack_b);
-		lstdelone(stack_b);
+		lstadd_front(stack_a, *stack_b);
+		lstdelone(*stack_b);
 		ft_putstr("pa\n");
 	}
 	else if (set == DO_B)
 	{
-		lstadd_front(&stack_b, stack_a);
-		lstdelone(stack_a);
+		lstadd_front(stack_b, *stack_a);
+		lstdelone(*stack_a);
 		ft_putstr("pb\n");
 	}
 }
 
-void	rotate(t_stack *stack_a, t_stack *stack_b, char set)
+void	rotate(t_stack **stack_a, t_stack **stack_b, char set)
 {
 	if (set == DO_A)
 	{
-
+		node_rotate(stack_a);
 		ft_putstr("ra\n");
 	}
 	else if (set == DO_B)
 	{
-
+		node_rotate(stack_b);
 		ft_putstr("rb\n");
 	}
 	else if (set == DO_ALL)
 	{
-		
+		node_rotate(stack_a);
+		node_rotate(stack_b);		
 		ft_putstr("rr\n");
 	}
 }
 
-void	reverse_rotate(t_stack *stack_a, t_stack *stack_b, char set)
+void	reverse_rotate(t_stack **stack_a, t_stack **stack_b, char set)
 {
 	if (set == DO_A)
 	{
-
+		node_reverse_rotate(stack_a);
 		ft_putstr("rra\n");
 	}
 	else if (set == DO_B)
 	{
-
+		node_reverse_rotate(stack_b);
 		ft_putstr("rrb\n");
 	}
 	else if (set == DO_ALL)
 	{
-
+		node_reverse_rotate(stack_a);
+		node_reverse_rotate(stack_b);
 		ft_putstr("rrr\n");
 	}
 }
