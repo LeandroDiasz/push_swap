0437290f7a3a315f597e018c6b48e6ac51a8d36a
ledias-d <leocraftjg@gmai.com>
2025-01-30 19:25:06 -0300

    fix(utils): correct node_swap implementation and add node_rotate, node_reverse_rotate, and assign_index functions

--- src/utils.c
+++ src/utils.c
@@ -26,3 +26,70 @@ int	lstsize(t_stack *lst)
 	}
 	return (i);
 }
+
+void	node_swap(t_stack **stack)
+{
+	t_stack *first;
+	t_stack *second;
+
+	if (!stack || !*stack || !(*stack)->next)
+			return ;
+	first = *stack;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	*stack = second;
+}
+
+void	node_rotate(t_stack **stack)
+{
+	t_stack *first;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	*stack = first->next;
+	first->next = NULL;
+	lstadd_back(stack, first);
+}
+
+void	node_reverse_rotate(t_stack **stack)
+{
+	t_stack *last;
+	t_stack	*second_last;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	last = *stack;
+	second_last = NULL;
+	while (last->next != NULL)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->next = *stack;
+	*stack = last;
+}
+
+void	assigin_index(t_stack *stack) 
+{
+	t_stack	*current;
+	t_stack	*iterator;
+	int		index;
+
+	current = stack;
+	while (current)
+	{
+		iterator = stack;
+		index = 0;
+		while (iterator)
+		{
+			if(current->content > iterator->content)
+				index++;
+			iterator = iterator->next;
+		}
+		current->index= index;
+		current = current->next;
+	}
+}
