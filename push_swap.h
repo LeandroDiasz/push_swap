0b2815051b6c40f95106c9aec69d748de7615f6a
ledias-d <leocraftjg@gmai.com>
2025-02-01 18:51:45 -0300

    style: adjust code to comply with Norminette

--- push_swap.h
+++ push_swap.h
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef PUSH_SWAP_H
+#ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 # define INT_MIN -2147483648
@@ -19,58 +19,58 @@
 # define DO_B 'b'
 # define DO_ALL 'c'
 
-# include "libft/libft.h"
 # include "libft/ft_printf/ft_printf.h"
-# include <stdio.h>
+# include "libft/libft.h"
 
 typedef struct s_stack
 {
 	int				content;
 	int				index;
 	struct s_stack	*next;
-}	t_stack;
+}					t_stack;
 
 /*--- main.c ---*/
-void	init_stack(t_stack **stack_a, t_stack **stack_b, char **av);
+void				init_stack(t_stack **stack_a, t_stack **stack_b, char **av);
 
 /*--- stack.c ---*/
-void	lstadd_front(t_stack **lst, t_stack *new);
-void	lstadd_back(t_stack **lst, t_stack *new);
-t_stack	*lstnew(int content);
-t_stack	*lstlast(t_stack *lst);
-void	lstdelone(t_stack *stack);
+void				lstadd_front(t_stack **lst, t_stack *new);
+void				lstadd_back(t_stack **lst, t_stack *new);
+t_stack				*lstnew(int content);
+t_stack				*lstlast(t_stack *lst);
+void				lstdelone(t_stack *stack);
 
 /*--- utils.c ---*/
-int		lstsize(t_stack *lst);
-void	node_rotate(t_stack **stack);
-void	node_swap(t_stack **stack);
-void	node_reverse_rotate(t_stack **stack);
-void	assigin_index(t_stack *stack);
+int					lstsize(t_stack *lst);
+void				node_rotate(t_stack **stack);
+void				node_swap(t_stack **stack);
+void				node_reverse_rotate(t_stack **stack);
+void				assigin_index(t_stack *stack);
 
 /*--- sort.c ---*/
-int		is_sorted(t_stack *stack);
-void	sort(t_stack **stack_a, t_stack **stack_b);
-void	medium_sort(t_stack **stack_a, t_stack **stack_b, int size);
-void	sort_three(t_stack **stack_a);
-void	sort_two(t_stack **stack_a);
+int					is_sorted(t_stack *stack);
+void				sort(t_stack **stack_a, t_stack **stack_b);
+void				medium_sort(t_stack **stack_a, t_stack **stack_b, int size);
+void				sort_three(t_stack **stack_a);
+void				sort_two(t_stack **stack_a);
 
 /*--- radix.c ---*/
-void	radix(t_stack *stack_a, t_stack *stack_b);
+void				radix(t_stack *stack_a, t_stack *stack_b);
 
 /*--- operations.c ---*/
-void	swap(t_stack **stack_a, t_stack **stack_b, char set);
-void	push(t_stack **stack_a, t_stack **stack_b, char set);
-void	rotate(t_stack **stack_a, t_stack **stack_b, char set);
-void	reverse_rotate(t_stack **stack_a, t_stack **stack_b, char set);
+void				swap(t_stack **stack_a, t_stack **stack_b, char set);
+void				push(t_stack **stack_a, t_stack **stack_b, char set);
+void				rotate(t_stack **stack_a, t_stack **stack_b, char set);
+void				reverse_rotate(t_stack **stack_a, t_stack **stack_b,
+						char set);
 
 /*--- erro.c ---*/
-void	free_stack(t_stack **stack);
-void	error(t_stack **stack);
-void    free_matrix(char **str);
+void				free_stack(t_stack **stack);
+void				error(t_stack **stack);
+void				free_matrix(char **str);
 
 /*--- verify_stack.c ---*/
-int		verify_duplicate(t_stack *stack_a, int nbr);
-long	ft_atol(const char *str);
-int		verify_sintax(char *av);
+int					verify_duplicate(t_stack *stack_a, int nbr);
+long				ft_atol(const char *str);
+int					verify_sintax(char *av);
 
 #endif
