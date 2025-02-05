ac5b75fde3294b90ca9fbf233e48a01c8eb2e4b9
ledias-d <leocraftjg@gmai.com>
2025-02-05 19:40:08 -0300

    feat: implement radix sort algorithm

--- src/radix.c
+++ src/radix.c
@@ -12,6 +12,30 @@
 
 #include "../push_swap.h"
 
-void	radix(t_stack *stack_a, t_stack *stack_b)
+void	radix(t_stack **stack_a, t_stack **stack_b, int size)
 {
+	int	i;
+	int	j;	
+	int	max_bits;
+
+	i = 0;
+	max_bits = 0;
+	assigin_index(*stack_a);
+	while((size - 1) >> max_bits != 0)
+		max_bits++;
+	while (i < max_bits)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if ((((*stack_a)->index >> i) & 1) == 0)
+				push(stack_a, stack_b, DO_B);
+			else
+				rotate(stack_a, NULL, DO_A);
+			j++;
+		}
+		while (lstsize(*stack_b) > 0)
+			push(stack_a, stack_b, DO_A);
+		i++;
+	}
 }
